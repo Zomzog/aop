abc4037158ff966cbbb738c9be7c981746f554c4
Thibault Duperron <zomzog@gmail.com>
2018-09-11 20:57:09 +0200

    Initial commit

---
+++ example/src/main/java/bzh/zomzog/prez/aop/Pony.java
@@ -0,0 +1,23 @@
+package bzh.zomzog.prez.aop;
+
+
+public class Pony {
+
+    private int distance = 0;
+
+    public boolean makeItWalk(int distance){
+        this.distance += distance;
+        return true;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        return "Pony{" +
+                "distance=" + distance +
+                '}';
+    }
+}
